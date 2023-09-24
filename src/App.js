933211a948bc48b52081303665476f966f50819e
pverma2905 <pverma2905@gmail.com>
2023-09-24 19:37:41 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Aside from './components/Aside';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Section from './components/Section';
+
+function App() {
+  return (
+    <>
+    <Header/>
+     <main>
+     <Aside/>
+     <Section/>
+    </main>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
